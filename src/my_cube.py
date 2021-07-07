473483c29d562660d4249c06f77d649a6e8cbbed
Krishna Reddy <Krishnareddyg1996@gmail.com>
2021-07-07 23:36:19 +0530

    adding a cube function

---
+++ src/my_cube.py
@@ -0,0 +1,16 @@
+def my_square(x):
+	""" takes a value and returns the squared value.
+	uses the ** opertor for squaring
+	"""
+	return x**3
+'''
+def my_square2(y):
+	"""takes a value and returns the squared value.
+	uses the * operator.
+	"""
+	return y*y
+'''
+print(my_square(42))
+'''
+print(my_square2(42))
+'''
